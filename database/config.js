2effa59f1f45f68b778781fae29cd25f950042dd
danieleugui <danieleugui@msn.com>
2024-01-16 10:21:47 -0500

    restoring my db connection

--- database/config.js
+++ database/config.js
@@ -1,49 +1,17 @@
 const mongoose = require('mongoose');
 
-const dbConnection = async() => {
 
+const dbConnection = async () => {
     try {
-        
-        await mongoose.connect( process.env.DB_CNN , {
-            useNewUrlParser: true, 
-            useUnifiedTopology: true,
-            useCreateIndex: true
-        });
 
-        console.log('DB Online');
-
+        await mongoose.connect(process.env.DB_CNN);
 
     } catch (error) {
         console.log(error);
-        throw new Error('Error a la hora de inicializar BD');
+        throw new Error('Error a la hora de inicializar la base de datos')
     }
-
-
 }
-
 
 module.exports = {
     dbConnection
 }
-
-// const mongoose = require('mongoose');
-
-
-// const dbConnection = async () => {
-//     try {
-
-//         await mongoose.connect(process.env.DB_CNN);
-
-//         // const Cat = mongoose.model('Cat', { name: String });
-
-//         // const kitty = new Cat({ name: 'Zildjian' });
-//         // kitty.save().then(() => console.log('meow'));
-
-//     } catch (error) {
-//         throw new Error('Error a la hora de inicializar la base de datos')
-//     }
-// }
-
-// module.exports = {
-//     dbConnection
-// }
