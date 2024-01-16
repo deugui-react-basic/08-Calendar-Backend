b53b6d9c586fb5e720a3cbc21050f04a3eaf8009
danieleugui <danieleugui@msn.com>
2024-01-16 09:53:16 -0500

    updating db connection

--- database/config.js
+++ database/config.js
@@ -1,21 +1,49 @@
 const mongoose = require('mongoose');
 
+const dbConnection = async() => {
 
-const dbConnection = async () => {
     try {
+        
+        await mongoose.connect( process.env.DB_CNN , {
+            useNewUrlParser: true, 
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        });
 
-        await mongoose.connect(process.env.DB_CNN);
+        console.log('DB Online');
 
-        // const Cat = mongoose.model('Cat', { name: String });
-
-        // const kitty = new Cat({ name: 'Zildjian' });
-        // kitty.save().then(() => console.log('meow'));
 
     } catch (error) {
-        throw new Error('Error a la hora de inicializar la base de datos')
+        console.log(error);
+        throw new Error('Error a la hora de inicializar BD');
     }
+
+
 }
+
 
 module.exports = {
     dbConnection
 }
+
+// const mongoose = require('mongoose');
+
+
+// const dbConnection = async () => {
+//     try {
+
+//         await mongoose.connect(process.env.DB_CNN);
+
+//         // const Cat = mongoose.model('Cat', { name: String });
+
+//         // const kitty = new Cat({ name: 'Zildjian' });
+//         // kitty.save().then(() => console.log('meow'));
+
+//     } catch (error) {
+//         throw new Error('Error a la hora de inicializar la base de datos')
+//     }
+// }
+
+// module.exports = {
+//     dbConnection
+// }
